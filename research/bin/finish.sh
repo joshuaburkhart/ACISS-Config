b7374e921747a256e4460b0ea31f185c406df219
Joshua Burkhart <jburkhar@uoregon.edu>
2012-05-15 13:46:14 -0700

    printing timestamp for started / finished jobs

--- research/bin/finish.sh
+++ research/bin/finish.sh
@@ -28,7 +28,7 @@ while true
 do
 
   qstat -n | grep jburkhar | awk '{$5=""; $10=""; $11=""; print $0}' > $TMP_FILE_1
-  diff $TMP_FILE_1 $TMP_FILE_2 | awk '{if($1==">") $1="FINISHED"; else if($1=="<") $1="STARTED"; else ; print $0}'
+  diff $TMP_FILE_1 $TMP_FILE_2 | awk '{if($1==">"){$1="<<FINISHD>>"; print d, $0;} else if($1=="<"){$1="<<STARTED>>"; print d, $0;}}' "d=$(date)"
   cat $TMP_FILE_1 > $TMP_FILE_2
   sleep 5
 
